26329631108b77f9c2a1ccfb66b4bd0ad11b0b44
Daniel W Cross <danielwincross94@gmail.com>
2022-06-15 14:55:28 +0700

    add initial Javascript. Javascript changes the h1 tage inner HTML

---
+++ index.js
@@ -0,0 +1,25 @@
+// Change the name in H1
+
+let titleName = document.querySelector('h1');
+
+let nameChange = function() {
+    document.querySelector('h1').innerHTML = 'Dan:)';
+}
+
+let nameReturn = function() {
+    document.querySelector('h1').innerHTML = 'Daniel Cross';
+}
+
+titleName.addEventListener('mouseover', nameChange);
+titleName.addEventListener('mouseout', nameReturn);
+
+
+// Increase Picture size when over and spin 
+
+// let picture = document.getElementsById('profile');
+
+// let pictureGrow = function () {
+//     picture.style.minWidth = '800px';
+// }
+
+// picture.addEventListener('mouseover', pictureGrow);
